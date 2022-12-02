bb30de18dd7dc0fda11606ebf96ad27f0688917f
Tim Parker <parker_tim@yahoo.co.uk>
2022-12-02 08:57:48 +0100

    Days 1 & 2

---
+++ 1/main.js
@@ -0,0 +1,27 @@
+fs = require('fs')
+
+data = fs.readFileSync('input.txt').toString()
+    .split("\n")
+    .map((calories => parseInt(calories)))
+
+elfCalorieTotals = []
+currentElfTotal = 0
+data.forEach(line => {
+    mealCalories = parseInt(line)
+    if(isNaN(mealCalories)) {
+        if(currentElfTotal > 0) {
+            elfCalorieTotals.push(currentElfTotal)
+        }
+        currentElfTotal = 0
+    } else {
+        currentElfTotal += mealCalories
+    }
+})
+
+maxElfCalories = Math.max(...elfCalorieTotals)
+elfWithMaxCalories = elfCalorieTotals.indexOf(maxElfCalories)
+console.log(`Part 1: ${maxElfCalories}`)
+
+elfCalorieTotals.sort((a, b) => b-a)
+firstThreeElvesCalories = elfCalorieTotals[0] + elfCalorieTotals[1] + elfCalorieTotals[2]
+console.log(`Part 2: ${firstThreeElvesCalories}`)
---
+++ 2/main.js
@@ -0,0 +1,44 @@
+fs = require('fs')
+
+ROCK_PLAYED = 1
+PAPER_PLAYED = 2
+SCISSORS_PLAYED = 3
+
+WIN = 6
+DRAW = 3
+LOSS = 0
+
+
+var PART_1_SCORING_SYSTEM = {
+    "A X": ROCK_PLAYED + DRAW,
+    "A Y": PAPER_PLAYED + WIN,
+    "A Z": SCISSORS_PLAYED + LOSS,
+    "B X": ROCK_PLAYED + LOSS,
+    "B Y": PAPER_PLAYED + DRAW,
+    "B Z": SCISSORS_PLAYED + WIN,
+    "C X": ROCK_PLAYED + WIN,
+    "C Y": PAPER_PLAYED + LOSS,
+    "C Z": SCISSORS_PLAYED + DRAW,
+}
+
+var PART_2_SCORING_SYSTEM = {
+    "A X": SCISSORS_PLAYED + LOSS,
+    "A Y": ROCK_PLAYED + DRAW,
+    "A Z": PAPER_PLAYED + WIN,
+    "B X": ROCK_PLAYED + LOSS,
+    "B Y": PAPER_PLAYED + DRAW,
+    "B Z": SCISSORS_PLAYED + WIN,
+    "C X": PAPER_PLAYED + LOSS,
+    "C Y": SCISSORS_PLAYED + DRAW,
+    "C Z": ROCK_PLAYED + WIN,
+}
+
+function run(scoringSystem) {
+    return fs.readFileSync('input.txt').toString()
+        .split("\n")
+        .map(game => scoringSystem[game])
+        .reduce((a, b) => a + b )
+}
+
+console.log(`Part 1: ${run(PART_1_SCORING_SYSTEM)}`)
+console.log(`Part 2: ${run(PART_2_SCORING_SYSTEM)}`)
